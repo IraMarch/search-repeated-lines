3b183da85c6f89f08aee1441b3953ff864ddfacc
IraMarch <61013545+IraMarch@users.noreply.github.com>
2020-02-14 12:50:20 +0300

    upload files
    upload main.go file and txt files for testing

---
+++ main.go
@@ -0,0 +1,72 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 10.
+//!+
+
+// Dup2 prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type countBody struct {
+	count int
+	filenames map[string]bool
+}
+
+func main() {
+	countmap := make(map[string]*countBody)
+	files := os.Args[1:]
+	//fmt.Println (os.Args)
+//	if len(files) == 0 {
+//		countLines(os.Stdin, countmap)
+//	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+
+			countLines(f, countmap,len(files))
+			f.Close()
+		}
+//	}
+	for line, n := range countmap {
+		if n.count > 1 {
+			fmt.Printf("%d\t%s\t", n.count, line)
+			for f := range n.filenames {
+				fmt.Printf("%s ", f)
+			}
+			fmt.Printf("\n")
+		}
+	}
+}
+
+func countLines(f *os.File, countmap map[string]*countBody, nfiles int) {
+	input := bufio.NewScanner(f)
+	fileinfo, err := f.Stat()
+	filename := fileinfo.Name()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+
+	}
+	for input.Scan() {
+		if _, ok  := countmap[input.Text()]; !ok {
+			countmap[input.Text()] = &countBody{0, make (map[string]bool)}
+		}
+			if !countmap[input.Text()].filenames[filename] {
+				countmap[input.Text()].filenames[filename]= true
+			}
+			countmap[input.Text()].count++
+
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
+
+//!-
